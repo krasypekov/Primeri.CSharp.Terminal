d32c0ec05b0f1316292d09ce60c3b2f87d61c830
Красимир Иванов <krasypekov@abv.bg>
2015-08-26 09:24:36 +0200

    Модул 02, Видео 07 - Работа с масиви

--- Modul02/TableProject/Program.cs
+++ Modul02/TableProject/Program.cs
@@ -18,7 +18,7 @@ namespace TableProject
 			Console.WriteLine ("Броят на масива:\n" + "1,2,3,4,5,6,7,8,9\n\nе:" + parse.Length);
 
 			string list1 = string.Join (";", parse);
-			Console.WriteLine ("Новият стринг е:\n" + list1 + "\n\n");
+			Console.WriteLine ("\n\nНовият стринг е:\n" + list1 + "\n\n");
 		}
 	}
 }

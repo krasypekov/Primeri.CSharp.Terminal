ea175e992bc7660c1f4e066e8b45c21eb0aab81f
Красимир Иванов <krasypekov@abv.bg>
2015-08-27 16:26:38 +0200

    Модул 03, Видео 05 - Булеви оператори

--- Modul03/BoolExplore/Program.cs
+++ Modul03/BoolExplore/Program.cs
@@ -6,7 +6,26 @@ namespace BoolExplore
 	{
 		public static void Main (string[] args)
 		{
-			Console.WriteLine ("Hello World!");
+//			bool _b = true;	//true / false
+//
+//			bool _test = 5 > 10;
+
+			int[] _i = new int[] { 4, 3, 2 };
+
+			int _ui = Convert.ToInt32 (Console.ReadLine ());
+
+			bool _check01 = _ui <= _i.Length;
+			bool _check02 = _ui > 0;
+
+			bool _exp01 = _check01 && _check02; //Логическо 'и'  "&&"
+			bool _exp02 = _check02 || _check02; //Логическо 'или' "\\"
+			bool _exp03 = !_check01;
+
+			Console.WriteLine ("Логическо И: " +  _exp01.ToString() );
+			Console.WriteLine ("Логическо ИЛИ: " +  _exp02.ToString() );
+			Console.WriteLine ("Отрицание на " + _check01.ToString () + ": " + _exp03);
+
+			//			Console.WriteLine ( _i [ _ui - 1] );
 		}
 	}
 }

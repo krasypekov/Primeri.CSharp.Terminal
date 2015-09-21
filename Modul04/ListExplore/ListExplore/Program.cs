c0220f7f63e9fc09eb28b19af6b493cd4c340ebe
Красимир Иванов <krasypekov@abv.bg>
2015-09-21 19:30:49 +0200

    Модул 04, Видео 08 - Примери с List

--- Modul04/ListExplore/ListExplore/Program.cs
+++ Modul04/ListExplore/ListExplore/Program.cs
@@ -17,50 +17,44 @@ namespace ListExplore
 
 			//Потребителски вход
 			do {
-				Console.Write ( "? ");
+				Console.Write ("? ");
 				_userinput = Console.ReadLine ();
 
 
 				//Добавяне на стойности
 				//add <int>
-				if ( _userinput.ToLower().Contains ("add")) 
-				{
-					try
-					{
+				if (_userinput.ToLower ().Contains ("add")) {
+					try {
 						int _add = 0;
 
-						if ( int.TryParse ( _userinput.Split (' ')[1], out _add ) )
-						{
-							_list.Add ( _add );
+						if (int.TryParse (_userinput.Split (' ') [1], out _add)) {
+							_list.Add (_add);
 						}
 
 						Console.WriteLine ();
-					}catch{}
+					} catch {}
 				}
 
 				//Преглед на List
-				if ( _userinput.ToLower().Contains ("show") )
-				{
+				if (_userinput.ToLower ().Contains ("show")) {
 					Console.Write (" Списък: ");
 
-					foreach (int value in _list)
-					{
-						Console.Write ( value.ToString () + ", ");
+					foreach (int value in _list) {
+						Console.Write (value.ToString () + ", ");
 					}
 
-					Console.WriteLine ("\n" );
+					Console.WriteLine ("\n");
 				}
 				//Размер на List
-				if ( _userinput.ToLower().Contains ("size") )
-				{
-					Console.WriteLine ("размер на списъка: " + _list.Count.ToString ( ) + "\n");
+				if (_userinput.ToLower ().Contains ("size")) {
+					Console.WriteLine ("размер на списъка: " + _list.Count.ToString () + "\n");
 				} 
 
-			
-			}while(_userinput != "exit");
+			} while(_userinput != "exit");
+		}
 	}
 }
-}
+
 
 	
 

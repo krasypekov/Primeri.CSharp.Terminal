3634e8373bea855d12ab23c5b703d36b47b70ee4
Красимир Иванов <krasypekov@abv.bg>
2015-09-24 15:56:45 +0200

    Модул 05, Видео 02 - Използване на референции в методите

--- Modul05/Modul05/Program.cs
+++ Modul05/Modul05/Program.cs
@@ -15,6 +15,33 @@ namespace Modul05
 				Console.Write ("$ ");		usercommand = Console.ReadLine ();
 				if (usercommand.Contains ( "cmd01" ) ) 	cmd01 ( usercommand ) ;
 				if (usercommand.Contains ( "cmd02" ) ) 	Console.WriteLine ( cmd02 ( usercommand ) ) ;
+
+				if (usercommand.Contains ( "cmd03" ) )
+				{
+					double _a = 0;
+
+					if ( cmd03( usercommand, out _a ) )
+					{
+						Console.WriteLine( "a.a = " + _a.ToString() );
+					}else{
+						Console.WriteLine ("Командата не е въведена коректно!");
+					}
+				}
+
+				if (usercommand.Contains ( "cmd04" ) ) 	
+				{
+					double _t = 5, _c = -2;
+					if ( cmd04 ( ref _t) )
+					{
+						Console.WriteLine ( _t);
+				
+					}
+					if (cmd04 ( ref _c))
+					{
+						Console.WriteLine ( 0 );				
+					}
+						
+				}
 					
 			}while ( usercommand != "exit" ); 
 
@@ -28,8 +55,7 @@ namespace Modul05
 				if (double.TryParse (_input.Split (' ') [1], out _temp)) 
 				{
 					Console.WriteLine ("_\nРезултат от " + _temp.ToString () + "^2: " + ( _temp * _temp).ToString() );
-				}
-
+					}
 
 			} catch {
 			}
@@ -41,9 +67,37 @@ namespace Modul05
 			if ( int.TryParse (_input.Split (' ') [1], out _temp)) 
 			{ 
 				_temp = _temp * _temp * _temp;
-
 			}
 			return _temp;
+		}
+	
+      public static bool cmd03 ( string _input, out double _i)  //cmd03 45 ---> 45*45  out
+{
+	try
+	{
+		string _p = _input.Split (' ')[1];  //cmd03, cmd03  43,  cmd03 ddd
+				double __p = 0;
+
+		if ( double.TryParse ( _p, out __p) )
+		{
+			_i = __p* __p;
+			return true;
+		}
+
+
+	}catch{
+	}
+
+	_i = 0;
+	return false;
+		}
+		public static bool cmd04 ( ref double _i )  //ref
+		{
+			double _temp = _i;
+			
+			_i = _i * _i;
+				
+			return ( _temp > 0 );
 		}
 	}
 }
